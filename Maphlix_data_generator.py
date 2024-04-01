d8ad22630ce69296af9f8569faafd1a3cd41b9bf
King-04 <kboateng396@gmail.com>
2024-04-01 23:07:33 +0800

    Data generated

---
+++ Maphlix_data_generator.py
@@ -0,0 +1,26 @@
+from faker import Faker
+import csv
+import random
+
+output = open('Maphlix_Trust_Ghana_Ltd_Data.CSV','w')
+fake = Faker()
+
+Company = "Maphlix Trust Ghana Ltd."
+Ghana_regions = ["Ashanti", "Brong-Ahafo", "Central", "Eastern", "Greater Accra", "Northern",
+                 "Upper East", "Upper West", "Volta", "Western", "Western North", "Ahafo", "Bono",
+                 "Bono East", "North East", "Savannah"]
+Services_list = ['Agricultural production', 'Agro-processing', 'Agronomy', 'Input supply']
+Demographics_list = ['Greater Accra', 'Volta']
+Customer_preference = 'Website'
+Communications_list = ['Email','Phone']
+
+header = ['Company', 'Name', 'Phone Number', 'Email Address', 'Address', 'Branch',
+          'Services / Products', 'Transaction Activity', 'Customer Preference', 'Communication Method']
+mywriter = csv.writer(output)
+mywriter.writerow(header)
+
+for r in range(120000):
+    mywriter.writerow([Company, fake.name(), fake.phone_number(), fake.email(), random.choice(Ghana_regions),
+                       random.choice(Demographics_list), random.choice(Services_list),
+                       fake.random_int(min=1,max=9999, step=1),
+                       Customer_preference, random.choice(Communications_list)])
